97f7cc91e606b5ec8d4a6039844370fdd95cdfe5
vitor12123 <vitorpallis@gmail.com>
2026-06-06 20:23:57 -0300

    fix: adicionando um boxplot que estava faltando

--- variabilidade.py
+++ variabilidade.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("current_catalog.csv")
 
@@ -15,3 +16,16 @@ for column in list_of_columns:
 #mostra numeros repitidos por coluna
 for i in range(len(tudo)):
     print(f"{tudo[i][0]}: {tudo[i][1]}")
+
+#grafico de barras que indica a quantidade de satélites por país
+nomes = df["country"].value_counts()[:20].index
+quantidade = df["country"].value_counts()[:20].values
+
+plt.boxplot(quantidade)
+
+plt.title("Quantidade de Satélites por País")
+plt.xlabel("País")
+plt.ylabel("Quantidade")
+
+
+plt.show()
